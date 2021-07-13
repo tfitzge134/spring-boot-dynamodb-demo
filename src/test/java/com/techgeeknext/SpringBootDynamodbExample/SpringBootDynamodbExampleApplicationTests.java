36d679aec6923c703a7ad42549b0e884cde3a879
Teresa <tfitzge134@gmail.com>
2021-07-13 12:52:50 -0500

    first commit

---
+++ src/test/java/com/techgeeknext/SpringBootDynamodbExample/SpringBootDynamodbExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techgeeknext.SpringBootDynamodbExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDynamodbExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
